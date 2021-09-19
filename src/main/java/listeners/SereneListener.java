f283bf857bb6d1306c9faeec87a22674c9746415
afarrukh125 <33581953+afarrukh125@users.noreply.github.com>
2021-09-19 18:27:11 +0100

    Complete experience bonus implementation

--- src/main/java/listeners/SereneListener.java
+++ src/main/java/listeners/SereneListener.java
@@ -1,5 +1,6 @@
 package listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
@@ -16,6 +17,16 @@ public class SereneListener implements Listener {
 
     @EventHandler
     public void onPlayerExperienceGain(PlayerExpChangeEvent playerExpChangeEvent) {
-        logger.info("Amount gained: " + playerExpChangeEvent.getAmount());
+        logger.info("Original amount gained: " + playerExpChangeEvent.getAmount());
+        Player player = playerExpChangeEvent.getPlayer();
+        int initialAmount = playerExpChangeEvent.getAmount();
+        int level = player.getLevel();
+        double bonus = Math.ceil((double) level / 4);
+        int finalAmount = initialAmount + (int) bonus;
+
+        if (level < 30) {
+            playerExpChangeEvent.setAmount(finalAmount);
+            logger.info("Actual experience gained is " + finalAmount + " from bonus.");
+        }
     }
 }

18114ab89a73edfcffbb4238bb68d6f31e208b71
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2022-05-16 00:02:57 +0100

    Rename logger

--- src/main/java/Serene.java
+++ src/main/java/Serene.java
@@ -7,13 +7,13 @@ import org.slf4j.LoggerFactory;
 
 public class Serene extends JavaPlugin {
 
-    private final Logger logger = LoggerFactory.getLogger(Serene.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Serene.class);
 
     @Override
     public void onEnable() {
         PluginManager pluginManager = getServer().getPluginManager();
         pluginManager.registerEvents(new SleepListener(), this);
         pluginManager.registerEvents(new ExperienceListener(), this);
-        logger.info("Started Serene...");
+        LOG.info("Started Serene...");
     }
 }

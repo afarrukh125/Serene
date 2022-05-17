3574eba60768c6be118ff636cb13e3f5c3573c1f
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2022-05-17 01:09:13 +0100

    Attempt another implementation of one-player sleep

--- src/main/java/listeners/SleepListener.java
+++ src/main/java/listeners/SleepListener.java
@@ -6,21 +6,24 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.util.Objects.requireNonNull;
 
 public class SleepListener implements Listener {
 
     private static final Logger LOG = LoggerFactory.getLogger(SleepListener.class);
-    AtomicBoolean hasSleepTriggered = new AtomicBoolean(false);
     private static final int FULL_DAY_TIME = 24000;
+
+    private AtomicReference<Player> atomicPlayerReference;
+
     public SleepListener() {
+        atomicPlayerReference = new AtomicReference<>();
     }
 
     public static long nextDayFullTime(long currentTime) {
@@ -28,33 +31,43 @@ public class SleepListener implements Listener {
         return (daysElapsed + 1) * FULL_DAY_TIME;
     }
 
-    // fixme Can't trigger from separate thread
     @EventHandler
-    public void onPlayerSleep(PlayerBedEnterEvent playerBedEnterEvent) {
-        if (playerBedEnterEvent.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK) && !hasSleepTriggered.get()) {
-            long startTime = System.nanoTime();
-            long now = System.nanoTime();
-            LOG.info("Starting count");
-            Location location = playerBedEnterEvent.getBed().getLocation();
-            notifyAllPlayersOfSleepEvent(playerBedEnterEvent, location);
-            long targetTimeInNanos = Duration.ofSeconds(2).toNanos();
-            while (now - startTime < targetTimeInNanos) {
-                now = System.nanoTime();
+    public void onPlayerEnterBed(PlayerBedEnterEvent playerBedEnterEvent) {
+        if (playerBedEnterEvent.getBedEnterResult().equals(PlayerBedEnterEvent.BedEnterResult.OK)) {
+            if (playerBedEnterEvent.getPlayer().getWorld().getPlayers().size() == 1)
+                return;
+            Player sleepingPlayer = playerBedEnterEvent.getPlayer();
+            List<Player> players = playerBedEnterEvent.getBed().getWorld().getPlayers();
+            for (Player player : players) {
+                if (!player.equals(sleepingPlayer))
+                    player.setSleepingIgnored(true);
             }
-            LOG.info("Ready to apply");
-            apply(playerBedEnterEvent);
-            hasSleepTriggered.set(false);
+            atomicPlayerReference.set(sleepingPlayer);
+            notifyAllPlayersOfSleepEvent(playerBedEnterEvent);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLeaveBed(PlayerBedLeaveEvent playerBedLeaveEvent) {
+        Player sleepingPlayer = playerBedLeaveEvent.getPlayer();
+        if (playerBedLeaveEvent.getPlayer().getWorld().getPlayers().size() == 1)
+            return;
+        if (playerBedLeaveEvent.isCancelled() && atomicPlayerReference.get().equals(sleepingPlayer)) {
+            atomicPlayerReference = new AtomicReference<>();
+            List<Player> players = playerBedLeaveEvent.getBed().getWorld().getPlayers();
+            for (Player player : players)
+                player.setSleepingIgnored(false);
         }
     }
 
 
-    private void notifyAllPlayersOfSleepEvent(PlayerBedEnterEvent playerBedEnterEvent, Location bedLocation) {
-        String sleepingPlayerName = playerBedEnterEvent.getPlayer().getName();
-        World world = requireNonNull(bedLocation.getWorld());
+    private void notifyAllPlayersOfSleepEvent(PlayerBedEnterEvent playerBedEnterEvent) {
+        Player sleepingPlayer = playerBedEnterEvent.getPlayer();
+        String sleepingPlayerName = sleepingPlayer.getName();
+        World world = requireNonNull(sleepingPlayer.getWorld());
         List<Player> players = world.getPlayers();
-        for (Player p : players) {
+        for (Player p : players)
             p.sendMessage(sleepingPlayerName + " has triggered sleep");
-        }
     }
 
     private void apply(PlayerBedEnterEvent playerBedEnterEvent) {

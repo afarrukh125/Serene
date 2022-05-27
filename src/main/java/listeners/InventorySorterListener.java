93f1bbe97d40a9f055b7850b459e0d8279d12401
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2022-05-27 15:57:14 +0100

    Order exceptions better in InventorySorterListener

--- src/main/java/listeners/InventorySorterListener.java
+++ src/main/java/listeners/InventorySorterListener.java
@@ -2,20 +2,34 @@ package listeners;
 
 import org.bukkit.Material;
 import org.bukkit.block.Chest;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static java.util.Objects.requireNonNull;
 
 public class InventorySorterListener implements Listener {
 
+    private static final Logger LOG = LoggerFactory.getLogger(InventorySorterListener.class);
+
     @EventHandler
     public void onChestOpenEvent(PlayerInteractEvent playerInteractEvent) {
-        boolean didClickChest = requireNonNull(playerInteractEvent.getClickedBlock()).getType().equals(Material.CHEST);
-        if (didClickChest) {
-            Chest chest = (Chest) playerInteractEvent.getClickedBlock();
-            // TODO do something with inventory
+        if (playerInteractEvent.getClickedBlock() != null) {
+            boolean wasChest = requireNonNull(playerInteractEvent.getClickedBlock()).getType().equals(Material.CHEST);
+            if (wasChest) {
+                boolean sneaking = playerInteractEvent.getPlayer().isSneaking();
+                boolean rightClicked = playerInteractEvent.getAction().equals(Action.RIGHT_CLICK_BLOCK);
+                boolean usedFeather = playerInteractEvent.hasItem() && requireNonNull(playerInteractEvent.getItem()).getType().equals(Material.FEATHER);
+                if (rightClicked && usedFeather && sneaking) {
+                    Chest chest = (Chest) playerInteractEvent.getClickedBlock().getState();
+                    // TODO do something with inventory
+                    BlockData blockData = chest.getBlockData();
+                }
+            }
         }
     }
 }

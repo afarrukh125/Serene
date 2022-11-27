5a145bd14aa9f6962882577f6040f74f60c87bd6
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2022-11-27 14:59:45 +0000

    Allow item search to look in ender chests if any are nearby

--- src/main/java/me/plugin/serene/actions/ItemSearcher.java
+++ src/main/java/me/plugin/serene/actions/ItemSearcher.java
@@ -12,10 +12,8 @@ import org.bukkit.inventory.Inventory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
@@ -35,56 +33,76 @@ public class ItemSearcher {
 
     public void searchItem(Player player, Location originalLocation, World world) {
         parseTargetMaterialFromParam(targetItemParam).ifPresentOrElse(targetMaterial -> {
+            boolean enderChestHasItem = player.getEnderChest().contains(targetMaterial);
             Queue<Location> locationsToSearch = new LinkedList<>();
-            List<Chest> foundChests = new ArrayList<>();
+            Set<Location> foundChestLocations = new HashSet<>();
             Set<Chest> seenChests = new HashSet<>();
             Set<Location> seenLocations = new HashSet<>();
             locationsToSearch.add(originalLocation);
 
             while (!locationsToSearch.isEmpty()) {
                 var nextLocation = locationsToSearch.poll();
-                if (seenLocations.contains(nextLocation))
+                if (seenLocations.contains(nextLocation)) {
                     continue;
+                }
                 Block block = nextLocation.getBlock();
-                boolean wasChest = requireNonNull(block.getType()).equals(Material.CHEST);
+                Material blockType = requireNonNull(block.getType());
+                boolean wasChest = blockType.equals(Material.CHEST);
                 if (wasChest) {
                     var chest = (Chest) block.getState();
                     Inventory inventory = chest.getInventory();
                     if (!inventory.isEmpty() && inventory.contains(targetMaterial) && !seenChests.contains(chest)) {
-                        foundChests.add(chest);
+                        foundChestLocations.add(chest.getLocation());
                         seenChests.add(chest);
                     }
                 }
 
+                if (blockType.equals(Material.ENDER_CHEST) && enderChestHasItem) {
+                    foundChestLocations.add(block.getLocation());
+                }
+
                 seenLocations.add(nextLocation);
 
-                if (Math.abs(nextLocation.getX() - originalLocation.getX()) < MAX_DISTANCE_TO_SEARCH &&
-                    Math.abs(nextLocation.getY() - originalLocation.getY()) < MAX_DISTANCE_TO_SEARCH &&
-                    Math.abs(nextLocation.getZ() - originalLocation.getZ()) < MAX_DISTANCE_TO_SEARCH) {
-                    locationsToSearch.add(new Location(world, nextLocation.getX() + 1, nextLocation.getY(), nextLocation.getZ()));
-                    locationsToSearch.add(new Location(world, nextLocation.getX() - 1, nextLocation.getY(), nextLocation.getZ()));
-                    locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY() + 1, nextLocation.getZ()));
-                    locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY() - 1, nextLocation.getZ()));
-                    locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY(), nextLocation.getZ() + 1));
-                    locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY(), nextLocation.getZ() - 1));
+                if (isInBoundsOfOriginalLocation(originalLocation, nextLocation)) {
+                    queueSurroundingLocations(world, locationsToSearch, nextLocation);
                 }
             }
 
-            if (!foundChests.isEmpty()) {
-                if (foundChests.stream().anyMatch(c -> (c.getLocation().getY() > player.getLocation().getY()) || (c.getLocation().getY() < player.getLocation().getY()))) {
+            if (!foundChestLocations.isEmpty()) {
+                if (foundChestLocations.stream().anyMatch(l -> (l.getY() > player.getLocation().getY()) || (l.getY() < player.getLocation().getY()))) {
                     player.sendMessage("Found chests with item %s (Marked with green particle effect, may be above/below you)".formatted(targetMaterial));
                 } else {
                     player.sendMessage("Found chests with item %s (Marked with green particle effect)".formatted(targetMaterial));
                 }
-                for (Chest chest : foundChests) {
-                    player.playEffect(chest.getLocation(), Effect.BONE_MEAL_USE, 35);
-                    player.playSound(chest.getLocation(), Sound.BLOCK_CHEST_LOCKED, 0.45f, 0.25f);
+                for (Location chestLocation : foundChestLocations) {
+                    playEffectsAtLocation(player, chestLocation);
                 }
             } else {
+
                 player.sendMessage("Found no chests nearby with item %s".formatted(targetMaterial));
             }
         }, () -> player.sendMessage("Could not match %s to an item. Ensure you spelt the name of the item correctly."
                 .formatted(targetItemParam)));
+    }
+
+    private static boolean isInBoundsOfOriginalLocation(Location originalLocation, Location nextLocation) {
+        return Math.abs(nextLocation.getX() - originalLocation.getX()) < MAX_DISTANCE_TO_SEARCH &&
+               Math.abs(nextLocation.getY() - originalLocation.getY()) < MAX_DISTANCE_TO_SEARCH &&
+               Math.abs(nextLocation.getZ() - originalLocation.getZ()) < MAX_DISTANCE_TO_SEARCH;
+    }
+
+    private static void queueSurroundingLocations(World world, Queue<Location> locationsToSearch, Location nextLocation) {
+        locationsToSearch.add(new Location(world, nextLocation.getX() + 1, nextLocation.getY(), nextLocation.getZ()));
+        locationsToSearch.add(new Location(world, nextLocation.getX() - 1, nextLocation.getY(), nextLocation.getZ()));
+        locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY() + 1, nextLocation.getZ()));
+        locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY() - 1, nextLocation.getZ()));
+        locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY(), nextLocation.getZ() + 1));
+        locationsToSearch.add(new Location(world, nextLocation.getX(), nextLocation.getY(), nextLocation.getZ() - 1));
+    }
+
+    private static void playEffectsAtLocation(Player player, Location location) {
+        player.playEffect(location, Effect.BONE_MEAL_USE, 35);
+        player.playSound(location, Sound.ITEM_BUNDLE_DROP_CONTENTS, 0.45f, 0.25f);
     }
 
     private Optional<Material> parseTargetMaterialFromParam(String targetItemParam) {

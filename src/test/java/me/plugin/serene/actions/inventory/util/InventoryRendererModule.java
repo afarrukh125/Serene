ad9efde1f528ac06cfb540d2d7f45b41bf3c85b4
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2025-05-24 11:45:50 +0300

    Pick image provider implementation based on internet access

--- src/test/java/me/plugin/serene/actions/inventory/util/InventoryRendererModule.java
+++ src/test/java/me/plugin/serene/actions/inventory/util/InventoryRendererModule.java
@@ -3,16 +3,32 @@ package me.plugin.serene.actions.inventory.util;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetAddress;
 
 public class InventoryRendererModule extends AbstractModule {
-
+    private static final Logger LOG = LoggerFactory.getLogger(InventoryRendererModule.class);
     @Singleton
     @Provides
     public ItemImageProvider itemImageProvider() {
-        return new OnlineItemImageProvider();
+        try {
+            boolean online = InetAddress.getByName("8.8.8.8").isReachable(3000);
+            if(online) {
+                LOG.info("Connected to the internet successfully, using live images from online");
+                return new OnlineItemImageProvider();
+            } else {
+                LOG.info("Could not connect to the internet, using local file-based provider");
+                return new FileBasedItemImageProvider();
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     @Singleton
